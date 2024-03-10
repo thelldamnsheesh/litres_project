d17c7ccc04ef22db7f1ceaf95813361c05c98dcd
Evgeny Zelenin <evgeny.globe@yandex.ru>
2024-03-11 00:01:40 +0300

    Добавил негативный api тест

--- tests/api_tests/test_search_api.py
+++ tests/api_tests/test_search_api.py
@@ -35,8 +35,25 @@ def test_get_search_audiobook_by_title():
 @allure.severity('normal')
 @allure.label('api')
 def test_get_search_book_by_title():
-    schema = os.path.join(SCHEMA_DIR, "unsuccessful_searching_book.json")
+    schema = os.path.join(SCHEMA_DIR, "searching_book.json")
     query = quote(book2.title)
+    api = '/search'
+    url = f'{base_url}{api}?limit=12&q={query}%&types={book2.query_type}'
+    response = api_get(url, headers=headers)
+
+    assert response.status_code == 200
+    validate(response.json(), load_schema(schema))
+    a = response.json()
+
+
+@allure.epic('API тесты. Поиск книги/аудиокниги')
+@allure.label("owner", "thelldamnsiiuu")
+@allure.tag('regress', 'api', 'book', 'negative')
+@allure.severity('normal')
+@allure.label('api')
+def test_get_search_book_by_title_negative_api():
+    schema = os.path.join(SCHEMA_DIR, "unsuccessful_searching_book.json")
+    query = 'dfvbaab SEFdfS '
     api = '/search'
     url = f'{base_url}{api}?limit=12&q={query}%&types={book2.query_type}'
     response = api_get(url, headers=headers)
